097009b69fa7c7cc904c76c1750d0b3c9bef1eb5
safwan-lewis <omarisa@lewisu.edu>
2018-11-05 14:04:22 -0600

    add twitter dm support - get direct messages

--- chat.py
+++ chat.py
@@ -1,3 +1,4 @@
+import sys
 import aioconsole
 import asyncio
 import click
@@ -8,6 +9,13 @@ from client.chat_client import (
     LoginConflictError,
     LoginError
 )
+from twitter.blk_client import TwitterDMClient
+
+consumer_key = 'ew0pdIp6isx0OxnRsLuTdgRMp'
+consumer_secret = 'YZNBeCwQOIZqj7dAgTmFykcwNWyTaf0QYYto6FgcEcRhafpcS9'
+access_token = '4027843701-Fqs7VUBD6KQsjT8cwDfgrVhlh54ivS2FHLefAa5'
+access_secret = 'mfMffOsw80FLx8KPjplogfytkBdE2KABGOeb656eKdAyT'
+
 
 async def display_msgs(chat_client):
     while True:
@@ -15,7 +23,7 @@ async def display_msgs(chat_client):
         print('\n\n\t\tRECEIVED MESSAGE: {}'.format(msg))
 
 
-async def handle_user_input(chat_client, loop):
+async def handle_user_input(chat_client, twitter_client, loop):
     while True:
         print('\n\n')
         print('< 1 > closes connection and quits')
@@ -23,6 +31,7 @@ async def handle_user_input(chat_client, loop):
         print('< 3 > login')
         print('< 4 > list rooms')
         print('< 5 > post message to a room')
+        print('< 6 > get twitter direct messages')
 
 
         print('\tchoice: ', end='', flush=True)
@@ -72,6 +81,14 @@ async def handle_user_input(chat_client, loop):
             except Exception as e:
                 print('error posting message {}'.format(e))
 
+        elif command == '6':
+            try:
+                for msg in twitter_client.list_dms():
+                    print(msg)
+
+            except Exception as e:
+                print('error getting direct messages {}'.format(e))
+
 
 @click.group()
 def cli():
@@ -87,8 +104,18 @@ def connect(host, port):
 
     loop.run_until_complete(chat_client._connect())
 
+    twitter_blk_client = TwitterDMClient(consumer_key=consumer_key,
+                                         consumer_secret=consumer_secret,
+                                         access_token=access_token,
+                                         access_secret=access_secret)
+    try:
+        twitter_blk_client.init_auth()
+    except Exception as e:
+        print('error authenticating with twitter API: {}'.format(e))
+        sys.exit(1)
+
     # display menu, wait for command from user, invoke method on client
-    asyncio.ensure_future(handle_user_input(chat_client=chat_client, loop=loop))
+    asyncio.ensure_future(handle_user_input(chat_client=chat_client, twitter_client = twitter_blk_client, loop=loop))
     asyncio.ensure_future(display_msgs(chat_client=chat_client))
 
     loop.run_forever()
